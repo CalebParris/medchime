ed6cb19cb4de8587fb90b8963d4bce94436a7739
Caleb Parris <calebparris@live.com>
2020-12-07 13:28:26 -0600

    Added Global Stylesheet

--- screens/appointments.js
+++ screens/appointments.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { globalStyles } from '../styles/global';
 
 export default function Appointments(){
     return(
-        <View style={styles.container}>
-            <Text>Appointments Screen</Text>
+        <View style={globalStyles.container}>
+            <Text style={globalStyles.titleText}>Appointments Screen</Text>
         </View>
     )
 }
--- screens/medList.js
+++ screens/medList.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { globalStyles } from '../styles/global';
 
 export default function MedList(){
     return(
-        <View style={styles.container}>
-            <Text>Medication List Screen</Text>
+        <View style={globalStyles.container}>
+            <Text style={globalStyles.titleText}>Medication List Screen</Text>
         </View>
     )
 }
-
-const styles = StyleSheet.create({
-    container: {
-        padding: 24
-    }
-})
