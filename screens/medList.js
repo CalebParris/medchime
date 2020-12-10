360643e73595a936e6ec70b76188a5c287a3428c
Caleb Parris <calebparris@live.com>
2020-12-10 12:18:42 -0600

    Added Modal and dummy test data

--- screens/medList.js
+++ screens/medList.js
@@ -1,11 +1,44 @@
-import React from 'react';
-import { Text, View } from 'react-native';
-import { globalStyles } from '../styles/global';
+import React, { useState } from "react";
+import { Text, View, FlatList, Button, Modal } from "react-native";
+import { globalStyles } from "../styles/global";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
-export default function MedList(){
-    return(
-        <View style={globalStyles.container}>
-            <Text style={globalStyles.titleText}>Medication List Screen</Text>
+export default function MedList() {
+  const [meds, setMeds] = useState([
+    { text: "med 1", key: 1 },
+    { text: "med 2", key: 2 },
+    { text: "med 3", key: 3 },
+    { text: "med 4", key: 4 },
+  ]);
+  const [modalOpen, setModalOpen] = useState(false);
+
+  return (
+    <View style={globalStyles.container}>
+      <Text style={globalStyles.titleText}>Medication List Screen</Text>
+      <Modal visible={modalOpen} animationType="slide">
+        <View style={globalStyles.modal}>
+          <MaterialCommunityIcons
+            name="close"
+            size={24}
+            onPress={() => setModalOpen(false)}
+            style={globalStyles.modalClose}
+          />
         </View>
-    )
+      </Modal>
+      <View style={globalStyles.list}>
+        <FlatList
+          data={meds}
+          renderItem={({ item }) => (
+            <Text style={globalStyles.med}>{item.text}</Text>
+          )}
+        />
+      </View>
+      <MaterialCommunityIcons
+        name="plus-circle"
+        size={40}
+        onPress={() => setModalOpen(true)}
+        style={globalStyles.modalOpen}
+      />
+    </View>
+  );
 }
--- styles/global.js
+++ styles/global.js
@@ -1,27 +1,56 @@
-import  { StyleSheet } from "react-native";
-import { color } from "react-native-reanimated";
+import { StyleSheet } from "react-native";
 
 export const globalStyles = StyleSheet.create({
-    container: {
-        flex: 1,
-        padding: 20
-    },
-    titleText: {
-        fontWeight: 'bold',
-        fontSize: 18,
-        color: 'black'
-    },
-    paragraph: {
-        marginVertical: 8,
-        lineHeight: 20,
-    },
-    header: {
-        backgroundColor: 'blue',
-        padding: 40,
-    },
-    headerText: {
-        color: 'white',
-        textAlign: 'center',
-        fontSize: 30
-    }
-    })
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+  titleText: {
+    fontWeight: "bold",
+    fontSize: 18,
+    color: "black",
+  },
+  paragraph: {
+    marginVertical: 8,
+    lineHeight: 20,
+  },
+  header: {
+    backgroundColor: "blue",
+    padding: 40,
+  },
+  headerText: {
+    color: "white",
+    textAlign: "center",
+    fontSize: 30,
+  },
+  med: {
+    padding: 16,
+    marginTop: 16,
+    borderColor: "#bbb",
+    borderWidth: 1,
+    borderStyle: "dashed",
+    borderRadius: 1,
+    borderRadius: 10,
+  },
+  list: {
+    flex: 1,
+  },
+  inut: {
+    marginBottom: 10,
+    paddingHorizontal: 8,
+    paddingVertical: 6,
+    borderBottomWidth: 1,
+    borderBottomColor: "#ddd",
+  },
+  modal: {
+    flex: 1,
+  },
+  modalOpen: {
+    color: "blue",
+    alignSelf: "flex-end",
+  },
+  modalClose: {
+    color: "red",
+    alignSelf: "flex-end",
+  },
+});
